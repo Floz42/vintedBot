6027436ed36fbed39b4441fb03cb8677e1bd93f0
Florian THIEBAUD <fthiebaud@weborama.com>
2025-06-12 14:56:06 +0200

    [UPDATE] vite config file / package.json

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,23 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import ElementPlus from 'unplugin-element-plus/vite'
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    ElementPlus({}),
+    vueJsx()
+  ],
+  server: {
+    port: 4200,
+    host: 'homebots.localhost'
+  },
+  resolve: {
+    alias: {
+      '~': path.resolve(__dirname, './'),
+      '@': path.resolve(__dirname, './src')
+    }
+  }
 })
